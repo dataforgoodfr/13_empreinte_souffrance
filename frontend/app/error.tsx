716b554d1a432d21370e7d52779ba48bb52d2b5c
LucBERTON <luc.jp.berton@gmail.com>
2025-03-30 15:15:30 +0200

    removing hard coded styles for testing purposes

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -9,10 +9,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   }, [error]);
 
   return (
-    <section
-      className="w-full bg-gradient-to-r from-blue-600 via-blue-500 to-indigo-500 h-screen flex items-center justify-around px-8 sm:px-16"
-      style={{ backgroundColor: 'red' }}
-    >
+    <section className="w-full bg-gradient-to-r from-blue-600 via-blue-500 to-indigo-500 h-screen flex items-center justify-around px-8 sm:px-16">
       <div>
         <h2>Something went wrong!</h2>
         <button className="bg-indigo-800 rounded-lg p-2 cursor-pointer" onClick={() => reset()}>
