594c392f78e39d875777ad47ca6c2d6f97a1b5a6
Mathis Dupuy <Akiat@users.noreply.github.com>
2025-04-15 19:09:49 +0200

    Fix tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -47,7 +47,7 @@ def sample_product_data() -> ProductData:
         ingredients_tags=[],
         ingredients=[],
         countries="fr",
-        countries_tags=["en:france", "en:united-states"],
+        countries_tags=["en:france"],
     )
 
 
