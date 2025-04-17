e0173decbd1dbd2eef02db776967cb173c0a3754
Laurent Basara <krasnikov@hotmail.com>
2025-04-17 18:36:28 +0200

    Restored calculate_egg_weight

--- backend/app/business/open_food_facts/egg_weight_calculator.py
+++ backend/app/business/open_food_facts/egg_weight_calculator.py
@@ -247,6 +247,17 @@ def calculate_egg_weight(product_data: ProductData) -> float:
     return egg_weight
 
 
+def calculate_egg_weight(product_data: ProductData) -> float:
+    """
+    Calculates the weight of eggs based on the product data.
+
+    Returns:
+        The egg weight if applicable.
+    """
+
+    return calculate_egg_weight_and_reason(product_data)[0]
+
+
 def extract_quantity_and_unit(text):
     """
     Extracts the first integer and optionally the first word
