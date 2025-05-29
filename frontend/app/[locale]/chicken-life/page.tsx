379111003bfc8347394e325aa087f7c25eb8fe0f
LucBERTON <luc.jp.berton@gmail.com>
2025-05-29 14:25:20 +0200

    Remove old unused components to fix pipeline check

--- frontend/app/[locale]/chicken-life/page.tsx
+++ frontend/app/[locale]/chicken-life/page.tsx
@@ -1,22 +1,7 @@
-import ContentWithImageSection from '@/app/[locale]/ui/general/content-with-image-section';
-
 export default async function Page() {
   return (
-    <>
-      <ContentWithImageSection
-        text_content="Big interesting data about hens"
-        image_url="tmp_chicken-image.webp"
-        image_description="Picture of a chicken"
-        image_position="left"
-        hide_image_on_small_screen={false}
-      />
-      <ContentWithImageSection
-        text_content="Here we will have a more detailed explanation about something interesting."
-        image_url="tmp_chicken-image.webp"
-        image_description="Picture of a chicken"
-        image_position="left"
-        hide_image_on_small_screen={false}
-      />
-    </>
+      <>
+          <div>CHICKEN LIFE PAGE</div>
+      </>
   );
 }
