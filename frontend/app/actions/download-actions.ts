694e4b0cd740846107801da0805b6e92816b6244
LucBERTON <76648347+LucBERTON@users.noreply.github.com>
2025-06-03 12:46:48 +0200

    Feat/sit web go further section (#62)
    * go further section
    
    * add server side actions (not functional) for get-further section + background color for whole body
    
    * Try to fix check types pipeline
    
    * Try to fix check types pipeline
    
    * Try to fix check types pipeline
    
    * add uncommited action files
    
    * comment console.log to pass pipeline eslint check

---
+++ frontend/app/actions/download-actions.ts
@@ -0,0 +1,6 @@
+'use server';
+
+export async function downloadMediaKit() {
+  // TODO
+  // console.log('downloadMediaKit');
+}
---
+++ frontend/app/actions/share-actions.ts
@@ -0,0 +1,6 @@
+'use server';
+
+export async function shareOnTwitter() {
+  // TODO
+  // console.log('shareOnTwitter');
+}
