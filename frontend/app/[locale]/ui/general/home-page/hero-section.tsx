5ec48812049bd93a26f696533f98f080293931dc
julien Deniaud <deniaudjulien0@gmail.com>
2025-06-07 14:45:23 +0200

    fix(i18n): correct scoped translation key usage

--- frontend/app/[locale]/ui/general/home-page/hero-section.tsx
+++ frontend/app/[locale]/ui/general/home-page/hero-section.tsx
@@ -1,20 +1,21 @@
-import { getScopedI18n } from '@/locales/server';
+import { getI18n } from '@/locales/server';
 
 export default async function HeroSection() {
-  const scopedT = await getScopedI18n('HeroSection');
-  const title: string = scopedT('title');
+  // const t = await getScopedI18n('HeroSection');
+  const t = await getI18n();
+  const title: string = t('HeroSection.title');
 
   return (
     <header className="relative h-screen w-full overflow-hidden flex items-center justify-end">
       <img
         src="chicken_header.png"
-        alt={scopedT('altPicture')}
+        alt={t('HeroSection.altPicture')}
         className="absolute inset-0 object-cover object-right transform scale-[1.3] origin-top-right"
       />
 
       <hgroup className="relative  flex flex-col justify-center items-start text-left p-4 max-w-[40rem]">
         <h1 className="text-5xl font-bold">
-          <span className="{scopedT('title_sentence.strong1')} ">
+          <span className="{t('title_sentence.strong1')} ">
             {/* translation map to add a colored dot on each of the letters to keep the style of the model */}
             {title.split('').map((lettre: string, index: number) => (
               <ColorText key={index} lettre={lettre} color={lettre === ' ' ? 'transparent' : lettreColor()} />
@@ -28,12 +29,12 @@ export default async function HeroSection() {
           </span>
         </h1>
         <p className="mt-4 text-[#FFE9E9] uppercase">
-          <span className="text-4xl font-bold">{scopedT('title_sentence.strong1')}</span>
-          <span className="text-4xl">{scopedT('title_sentence.part1')}</span>
-          <span className="text-4xl font-bold">{scopedT('title_sentence.strong2')}</span>
+          <span className="text-4xl font-bold">{t('HeroSection.title_sentence.strong1')}</span>
+          <span className="text-4xl">{t('HeroSection.title_sentence.part1')}</span>
+          <span className="text-4xl font-bold">{t('HeroSection.title_sentence.strong2')}</span>
           <br />
-          <span className="text-4xl">{scopedT('title_sentence.part2')}</span>
-          <span className="text-4xl font-bold">{scopedT('title_sentence.strong3')}</span>
+          <span className="text-4xl">{t('HeroSection.title_sentence.part2')}</span>
+          <span className="text-4xl font-bold">{t('HeroSection.title_sentence.strong3')}</span>
         </p>
         <img src="/arrow_down.png" alt="Flèche vers le bas" className="mt-10 w-10 h-10 animate-bounce" />
       </hgroup>
