b9f7b5fceb1d85da2f4231e21c475446fde09cb8
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-06-12 16:36:12 +0200

    Update page.tsx
    Appel du composant WFI Section

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <WFISection />
       <LinkSection />
       <SufferingCausesSection />
       <PainEquationSection />
