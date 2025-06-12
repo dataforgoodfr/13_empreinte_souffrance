b63ec642965314d13cda0ed685361395003aca4a
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-06-12 16:29:01 +0200

    clean every unsued pages or components (#74)

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,13 +1,8 @@
 import HeroSection from '@/app/[locale]/ui/general/home-page/hero-section';
-// import { LocaleSelect } from './ui/localselect';
-import WFISection from '@/app/[locale]/ui/general/home-page/WFI-section';
 import SufferingCausesSection from '@/app/[locale]/ui/general/home-page/suffering-causes-section';
 import PainEquationSection from '@/app/[locale]/ui/general/home-page/pain-equation-section';
 import ResultsSection from '@/app/[locale]/ui/general/home-page/results-section';
-import HumanFoodSection from '@/app/[locale]/ui/general/home-page/human-food-section';
-import CallToActionSection from '@/app/[locale]/ui/general/home-page/call-to-action-section';
 import BookAnnouncementSection from '@/app/[locale]/ui/general/home-page/book-announcement-section';
-import MethodologySection from '@/app/[locale]/ui/general/home-page/methodology-section';
 import GoFurtherSection from '@/app/[locale]/ui/general/home-page/go-further-section';
 import LinkSection from '@/app/[locale]/ui/general/home-page/link-section';
 
@@ -16,13 +11,9 @@ export default function Home() {
     <>
       <HeroSection />
       <LinkSection />
-      <WFISection />
       <SufferingCausesSection />
       <PainEquationSection />
       <ResultsSection />
-      <HumanFoodSection />
-      <MethodologySection />
-      <CallToActionSection />
       <GoFurtherSection />
       <BookAnnouncementSection />
     </>
