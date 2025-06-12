7fa3c09e570953b7a42fe2bb8b1f2af6a92a4b61
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-06-12 17:57:15 +0200

    fix language selector css (#77)

--- frontend/app/[locale]/ui/localselect.tsx
+++ frontend/app/[locale]/ui/localselect.tsx
@@ -6,13 +6,46 @@ export const LocaleSelect = () => {
   const changeLocale = useChangeLocale();
 
   return (
-    <select
-      className="bg-green-300 text-gray-800 px-2 md:px-6 py-3 text-lg font-medium rounded-lg shadow-lg hover:bg-green-400"
-      value={locale}
-      onChange={(e) => changeLocale(e.target.value as 'fr' | 'en')}
-    >
-      <option value="en">English</option>
-      <option value="fr">Français</option>
-    </select>
+    <fieldset className="flex gap-2 bg-transparent text-[#3b0a0a]  text-sm font-medium rounded-lg  ">
+
+      <div>
+        <input
+          type="radio"
+          id="fr"
+          name="locale"
+          value="fr"
+          checked={locale === 'fr'}
+          onChange={() => changeLocale('fr')}
+          className="peer hidden"
+        />
+        <label
+          htmlFor="fr"
+          className="select-none cursor-pointer px-2 py-2 rounded-lg hover:outline peer-checked:bg-[#ff7f7f] transition-all"
+        
+        >
+          FR
+        </label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          id="en"
+          name="locale"
+          value="en"
+          checked={locale === 'en'}
+          onChange={() => changeLocale('en')}
+          className="peer hidden"
+        />
+        <label
+          htmlFor="en"
+          className="select-none cursor-pointer px-2 py-2 rounded-lg hover:outline peer-checked:bg-[#ff7f7f] transition-all"
+        >
+          EN
+        </label>
+      </div>
+
+
+    </fieldset>
   );
 };
