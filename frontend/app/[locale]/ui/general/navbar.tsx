6a7c9dcbb588063fa12d4d884e53c66f88721c14
julien44830 <deniaudjulien0@gmail.com>
2025-06-17 14:51:21 +0200

    comment deletion

--- frontend/app/[locale]/ui/general/navbar.tsx
+++ frontend/app/[locale]/ui/general/navbar.tsx
@@ -11,18 +11,15 @@ export default async function Navbar() {
     <>
       <header className="bg-red-50 p-4 w-full">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full gap-4">
-          {/* Ligne du haut : gauche = boutons + langue en mobile / boutons seul en desktop */}
           <div className="flex justify-between md:justify-start items-center gap-2 w-full md:w-auto">
             <div className="flex justify-start items-center gap-2">
               <Logo />
             </div>
-            {/* Ce bloc est visible en mobile seulement */}
             <div className="md:hidden">
               <LocaleSelect />
             </div>
           </div>
 
-          {/* Centre : liens */}
           <div className="flex justify-center items-center md:justify-end gap-4 font-mono font-black uppercase text-sm flex-grow basis-0 min-w-0">
             <Link href="" className="hover:bg-gray-200 rounded-full px-3 transition tracking-wider">
               {t('Navbar.link1')}
@@ -38,7 +35,6 @@ export default async function Navbar() {
             </Link>
           </div>
 
-          {/* Droite : sélecteur de langue visible en desktop seulement */}
           <div className="hidden md:flex justify-end">
             <LocaleSelect />
           </div>
