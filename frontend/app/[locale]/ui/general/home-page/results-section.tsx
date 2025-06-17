04e7c90e7cb52cc6309fcfd3821fa3102d429764
TheoDegorre <degorret@gmail.com>
2025-06-17 15:41:18 +0200

    fix scroll position

--- frontend/app/[locale]/ui/general/home-page/results-section.tsx
+++ frontend/app/[locale]/ui/general/home-page/results-section.tsx
@@ -6,7 +6,7 @@ export default async function ResultsSection() {
   const t = await getI18n();
 
   return (
-    <section id="ResultSection" className="min-h-screen py-8 w-full max-w-screen-xl mx-auto px-4" aria-labelledby="results-heading">
+    <section id="ResultSection" className="min-h-screen py-8 w-full max-w-screen-xl mx-auto px-4 scroll-mt-18" aria-labelledby="results-heading">
       <SectionHeading title={t('Results.title')} heading_number="3" />
       <div className="flex flex-col justify-center items-center">
         <div className="border-1 border-[#ff7f7f] w-full md:5/6 lg:w-4/6">
