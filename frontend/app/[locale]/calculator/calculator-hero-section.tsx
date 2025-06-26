54b122a4336c63b698d3b87ccf59feb18adeedb6
TheoDegorre <degorret@gmail.com>
2025-06-26 17:35:39 +0200

    fix css of calculator-hero-section

--- frontend/app/[locale]/calculator/calculator-hero-section.tsx
+++ frontend/app/[locale]/calculator/calculator-hero-section.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 export default async function CalculatorHeroSection() {
   const scopedT = await getScopedI18n('calculatorPage');
   return (
-    <section className="scroll-mt-18 max-w-screen  p-8 sm:px-16 w-full bg-white flex flex-col gap-16">
+    <section className="scroll-mt-18 max-w-screen p-8 sm:px-16 w-full bg-white flex flex-col justify-center items center gap-16">
       <h1 className="w-full text-3xl lg:text-5xl font-bold text-[#3b0a0a] text-left">{scopedT('descriptionSection.title')}</h1>
-      <figure className = "flex justify-evenly w-full gap-6">
+      <figure className = "flex justify-evenly w-full gap-6 flex-col sm:flex-row items-center">
         <Image
           src="/eggs_box.jpeg"
           width={540}
@@ -14,7 +14,7 @@ export default async function CalculatorHeroSection() {
           alt={scopedT('descriptionSection.imageAlt')}
           className="block rounded"
         />
-        <figcaption className="m-32 text-2xl text-[#3b0a0a] ">{scopedT('descriptionSection.figcaption')}</figcaption>
+        <figcaption className=" text-2xl flex sm:max-w-1/3 items-center text-[#3b0a0a] ">{scopedT('descriptionSection.figcaption')}</figcaption>
       </figure>
     </section>
   );
