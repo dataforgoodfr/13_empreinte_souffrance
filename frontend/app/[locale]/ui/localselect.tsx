38f1ce242ec723caa48513abbdb210f5e890f1d5
LucBERTON <76648347+LucBERTON@users.noreply.github.com>
2025-06-30 14:42:48 +0200

    Fix/header navbar responsiveness (#115)
    * on going navbar responsiveness improvements
    
    * navbar responsiveness for all size of screens

--- frontend/app/[locale]/ui/localselect.tsx
+++ frontend/app/[locale]/ui/localselect.tsx
@@ -5,47 +5,34 @@ export const LocaleSelect = () => {
   const locale = useCurrentLocale();
   const changeLocale = useChangeLocale();
 
+  const localesNames:  ('fr' |'en')[] = ['fr', 'en']
+
   return (
     <fieldset className="flex gap-2 bg-transparent text-[#3b0a0a]  text-sm font-medium rounded-lg  ">
 
-      <div>
-        <input
-          type="radio"
-          id="fr"
-          name="locale"
-          value="fr"
-          checked={locale === 'fr'}
-          onChange={() => changeLocale('fr')}
-          className="peer hidden"
-        />
-        <label
-          htmlFor="fr"
-          className="select-none cursor-pointer px-2 py-2 rounded-lg hover:outline peer-checked:bg-[#ff7f7f] transition-all"
-        
-        >
-          FR
-        </label>
-      </div>
+      { localesNames.map((localeName) =>
+      {
+        return (
+            <div key={localeName}>
+              <input
+                  type="radio"
+                  id={localeName}
+                  name="locale"
+                  value={localeName}
+                  checked={locale === localeName}
+                  onChange={() => changeLocale(localeName)}
+                  className="peer hidden"
+              />
+              <label
+                  htmlFor={localeName}
+                  className="select-none cursor-pointer px-2 py-2 rounded-lg hover:outline peer-checked:bg-[#ff7f7f] transition-all"
 
-      <div>
-        <input
-          type="radio"
-          id="en"
-          name="locale"
-          value="en"
-          checked={locale === 'en'}
-          onChange={() => changeLocale('en')}
-          className="peer hidden"
-        />
-        <label
-          htmlFor="en"
-          className="select-none cursor-pointer px-2 py-2 rounded-lg hover:outline peer-checked:bg-[#ff7f7f] transition-all"
-        >
-          EN
-        </label>
-      </div>
-
-
+              >
+                {localeName.toUpperCase()}
+              </label>
+            </div>
+        )
+      })}
     </fieldset>
   );
 };
