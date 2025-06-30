cc011a518df2c416e06665c23ab939271a028753
julien44830 <deniaudjulien0@gmail.com>
2025-06-30 17:28:52 +0200

    fix h1 to uupercase

--- frontend/app/[locale]/calculator/calculator-hero-section.tsx
+++ frontend/app/[locale]/calculator/calculator-hero-section.tsx
@@ -5,8 +5,10 @@ export default async function CalculatorHeroSection() {
   const scopedT = await getScopedI18n('calculatorPage');
   return (
     <section className="scroll-mt-18 max-w-screen p-8 sm:px-16 w-full bg-white flex flex-col justify-center items center gap-16">
-      <h1 className="w-full text-3xl lg:text-5xl font-bold text-[#3b0a0a] text-left">{scopedT('descriptionSection.title')}</h1>
-      <figure className = "flex justify-evenly w-full gap-6 flex-col sm:flex-row items-center">
+      <h1 className="w-full text-3xl lg:text-5xl font-bold text-[#3b0a0a] text-left uppercase">
+        {scopedT('descriptionSection.title')}
+      </h1>
+      <figure className="flex justify-evenly w-full gap-6 flex-col sm:flex-row items-center">
         <Image
           src="/eggs_box.jpeg"
           width={540}
@@ -14,7 +16,9 @@ export default async function CalculatorHeroSection() {
           alt={scopedT('descriptionSection.imageAlt')}
           className="block rounded"
         />
-        <figcaption className=" text-2xl flex sm:max-w-1/3 items-center text-[#3b0a0a] ">{scopedT('descriptionSection.figcaption')}</figcaption>
+        <figcaption className=" text-2xl flex sm:max-w-1/3 items-center text-[#3b0a0a] ">
+          {scopedT('descriptionSection.figcaption')}
+        </figcaption>
       </figure>
     </section>
   );
