2aa61226ee98a84ade156a00247494b30140dbd7
julien44830 <deniaudjulien0@gmail.com>
2025-07-01 15:09:55 +0200

    fix order to component

--- frontend/app/[locale]/methodology/method-details-section.tsx
+++ frontend/app/[locale]/methodology/method-details-section.tsx
@@ -5,8 +5,8 @@ import GlobalSufferingFigure from './global-suffering-figure';
 export default function MethodDetailsSection() {
   return (
     <>
-      <GlobalSufferingFigure />
       <QuantifySufferingByPain />
+      <GlobalSufferingFigure />
     </>
-  )
+  );
 }
