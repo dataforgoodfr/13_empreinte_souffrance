3456a8a607a553a3dd25a2b58a83296ba1c2e647
julien44830 <deniaudjulien0@gmail.com>
2025-07-01 15:14:07 +0200

    fix add target _blanc to redirect external link

--- frontend/app/[locale]/calculator/calculator-link-section.tsx
+++ frontend/app/[locale]/calculator/calculator-link-section.tsx
@@ -9,16 +9,18 @@ export default async function CalculatorLinkSection() {
       <h1 className=" w-full text-3xl lg:text-5xl font-bold text-[#3b0a0a] text-center text-wrap">
         {scopedT('linkSection')}
       </h1>
-      <a href="https://fr.openfoodfacts.org/cgi/search.pl?search_terms=boites+d%27oeufs&search_simple=1&action=process">
-
-      <Image
-        src="/off-logo-horizontal-light.svg"
-        width={540}
-        height={740}
-        alt={scopedT('descriptionSection.imageAlt')}
-        className="bg-white block p-2 hover:shadow-[0px_3px_8px_#000000]"
+      <a
+        target="_blank"
+        href="https://fr.openfoodfacts.org/cgi/search.pl?search_terms=boites+d%27oeufs&search_simple=1&action=process"
+      >
+        <Image
+          src="/off-logo-horizontal-light.svg"
+          width={540}
+          height={740}
+          alt={scopedT('descriptionSection.imageAlt')}
+          className="bg-white block p-2 hover:shadow-[0px_3px_8px_#000000]"
         />
-        </a>
+      </a>
     </section>
   );
 }
