ebab26f8aa05ec98cc63219457968b389b33f2c1
TheoDegorre <degorret@gmail.com>
2025-07-01 15:55:02 +0200

    hero banner section removed from top pages

--- frontend/app/[locale]/methodology/page.tsx
+++ frontend/app/[locale]/methodology/page.tsx
@@ -1,18 +1,16 @@
-import { getI18n } from '@/locales/server';
 import SufferingQuantificationSteps from './suffering-quantification-steps';
 import IntroductionSection from './introduction-section';
 import MethodDetailsSection from './method-details-section';
 import KeyResultsSection from './key-results-section';
-import HeroBanner from '../ui/general/heroBanner';
+
 import GoFurtherSection from '@/app/[locale]/ui/general/home-page/go-further-section';
 
 
 export default async function MethodologyPage() {
-  const t = await getI18n();
+
 
   return (
     <>
-      <HeroBanner title={t('MethodologyPage.heroiBanner.title')} />
       <IntroductionSection />
       <SufferingQuantificationSteps />
       <MethodDetailsSection />
