994d2a9d3223a0c507ab0d2c27b8609ab13e338a
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-07-01 14:29:36 +0200

    fix css of calculator-hero-section (#112)
    * fix css of calculator-hero-section
    
    * fix h1 to uupercase
    
    * fix minor responcive ratio img/text
    
    ---------
    
    Co-authored-by: julien44830 <deniaudjulien0@gmail.com>
    Co-authored-by: Julien DENIAUD <98946443+julien44830@users.noreply.github.com>

--- frontend/app/[locale]/calculator/calculator-hero-section.tsx
+++ frontend/app/[locale]/calculator/calculator-hero-section.tsx
@@ -4,17 +4,25 @@ import Image from 'next/image';
 export default async function CalculatorHeroSection() {
   const scopedT = await getScopedI18n('calculatorPage');
   return (
-    <section className="scroll-mt-18 max-w-screen p-8 sm:px-16 w-full bg-white flex flex-col items-center gap-16">
-      <h1 className="w-full text-3xl lg:text-5xl font-bold text-[#3b0a0a] text-left">{scopedT('descriptionSection.title')}</h1>
-      <figure className = "flex flex-col sm:flex-row max-w-4/5 gap-6">
+
+    <section className="scroll-mt-18 max-w-screen p-8 sm:px-16 w-full bg-white flex flex-col justify-center items center gap-16">
+      <h1 className="w-full text-3xl lg:text-5xl font-bold text-[#3b0a0a] text-left uppercase">
+        {scopedT('descriptionSection.title')}
+      </h1>
+      <figure className="flex justify-evenly w-full  flex-col sm:flex-row items-center">
+
         <Image
           src="/eggs_box.jpeg"
           width={540}
           height={740}
           alt={scopedT('descriptionSection.imageAlt')}
-          className="block rounded"
+          className="block rounded w-2/4"
         />
-        <figcaption className="text-2xl sm:m-32 text-[#3b0a0a] ">{scopedT('descriptionSection.figcaption')}</figcaption>
+
+        <figcaption className=" text-2xl flex w-full sm:w-1/3 items-center text-[#3b0a0a] ">
+          {scopedT('descriptionSection.figcaption')}
+        </figcaption>
+
       </figure>
     </section>
   );
