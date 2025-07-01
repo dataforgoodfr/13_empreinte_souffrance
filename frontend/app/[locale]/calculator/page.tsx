f0096b29cc056334c2c116d796f58c548117ee9a
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-07-01 16:20:31 +0200

    hero banner section removed from top pages (#129)

--- frontend/app/[locale]/calculator/page.tsx
+++ frontend/app/[locale]/calculator/page.tsx
@@ -1,15 +1,13 @@
-import { getI18n } from '@/locales/server';
 import CalculatorHeroSection from './calculator-hero-section';
 import CalculatorLinkSection from './calculator-link-section';
 import SelectorSection from './calculator-selector-section';
-import HeroBanner from '../ui/general/heroBanner';
+
 
 export default async function Calculator() {
-  const t = await getI18n();
+
 
   return (
     <>
-      <HeroBanner title={t('calculatorPage.heroBanner.title')} />
       <CalculatorHeroSection />
       <SelectorSection />
       <CalculatorLinkSection />
