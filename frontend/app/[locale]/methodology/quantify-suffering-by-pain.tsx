598db02bdd89d9bc2e44330ba93dbb67e3411149
LucBERTON <luc.jp.berton@gmail.com>
2025-07-14 10:24:37 +0200

    creation of button link component + refactoring of components with buttons

--- frontend/app/[locale]/methodology/quantify-suffering-by-pain.tsx
+++ frontend/app/[locale]/methodology/quantify-suffering-by-pain.tsx
@@ -1,118 +1,121 @@
 import { getI18n } from '@/locales/server';
-import Link from "next/link";
+import CustomLinkButton from "@/app/[locale]/ui/general/elements/custom-link-button";
 
 export default async function QuantifySufferingByPain() {
-  const t = await getI18n();
+    const t = await getI18n();
 
-  return (
-    <div className="w-full max-w-screen-xl mx-auto mt-12 dark-text">
-      <div className="flex flex-col p-6  sm:p-20 lg:p-0 md:w-2/3 m-auto">
-        <h2 className="text-2xl font-extrabold mb-4 uppercase ">
-          {t('MethodologyPage.QuantifySufferingByPain.title')}
-        </h2>
-        <p className="text-md mb-6">{t('MethodologyPage.QuantifySufferingByPain.question')}</p>
-        <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description1')}</p>
-        <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description2')}</p>
-        <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description3')}</p>
-        <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description4')}</p>
-      </div>
+    return (
+        <div className="w-full max-w-screen-xl mx-auto mt-12 dark-text">
+            <div className="flex flex-col p-6  sm:p-20 lg:p-0 md:w-2/3 m-auto">
+                <h2 className="text-2xl font-extrabold mb-4 uppercase ">
+                    {t('MethodologyPage.QuantifySufferingByPain.title')}
+                </h2>
+                <p className="text-md mb-6">{t('MethodologyPage.QuantifySufferingByPain.question')}</p>
+                <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description1')}</p>
+                <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description2')}</p>
+                <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description3')}</p>
+                <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description4')}</p>
+            </div>
 
-      <div className="p-6  w-full lg:max-w-4xl lg:mx-auto">
-        <h3 className="text-xl font-bold mb-4  uppercase">
-          {t('MethodologyPage.QuantifySufferingByPain.painTrackTableTitle')}
-        </h3>
-        <hr className="border-1 mb-10 border-pink-3" />
-        <div className="mt-12  overflow-x-auto w-full">
-          <table className="w-full text-sm text-center mb-6   border-separate border-spacing-0.5">
-            <thead>
-              <tr className="bg-grey h-12">
-                <th className="py-2 px-3 ">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.ruptureTissu')}
-                </th>
-                <th className="py-2 px-3 ">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.coagulation')}
-                </th>
-                <th className="py-2 px-3 ">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.inflammation')}
-                </th>
-                <th className="py-2 px-3 ">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.proliferation')}
-                </th>
-                <th className="py-2 px-3 ">{t('MethodologyPage.QuantifySufferingByPain.painTrackTable.total')}</th>
-                <th className="py-2 px-3 ">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.intensityOfSuffering')} de la souffrance
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="bg-grey  h-12">
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 ">70%</td>
-                <td className="py-2 px-3 ">196.00</td>
-                <td className="py-2 px-3  bg-pink-1">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.discomfort')}
-                </td>
-              </tr>
-              <tr className="bg-grey  h-12">
-                <td className="py-2 px-3 ">50%</td>
-                <td className="py-2 px-3 ">80%</td>
-                <td className="py-2 px-3 ">100%</td>
-                <td className="py-2 px-3 ">30%</td>
-                <td className="py-2 px-3 ">196.00</td>
-                <td className="py-2 px-3  bg-pink-2">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.pain')}
-                </td>
-              </tr>
-              <tr className="bg-grey  h-12">
-                <td className="py-2 px-3 ">50%</td>
-                <td className="py-2 px-3 ">20%</td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 ">196.00</td>
-                <td className="py-2 px-3  bg-pink-3">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.suffering')}
-                </td>
-              </tr>
-              <tr className="bg-grey  h-12">
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 "></td>
-                <td className="py-2 px-3 ">0 min</td>
-                <td className="py-2 px-3  bg-brown  light-text">
-                  {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.agony')}
-                </td>
-              </tr>
-              <tr className="">
-                <td className="py-2 px-3 ">0,5–2 min</td>
-                <td className="py-2 px-3 ">5–15 min</td>
-                <td className="py-2 px-3 ">16–48h</td>
-                <td className="py-2 px-3 ">224–36h</td>
-              </tr>
-            </tbody>
-          </table>
+            <div className="p-6  w-full lg:max-w-4xl lg:mx-auto">
+                <h3 className="text-xl font-bold mb-4  uppercase">
+                    {t('MethodologyPage.QuantifySufferingByPain.painTrackTableTitle')}
+                </h3>
+                <hr className="border-1 mb-10 border-pink-3"/>
+                <div className="mt-12  overflow-x-auto w-full">
+                    <table className="w-full text-sm text-center mb-6   border-separate border-spacing-0.5">
+                        <thead>
+                        <tr className="bg-grey h-12">
+                            <th className="py-2 px-3 ">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.ruptureTissu')}
+                            </th>
+                            <th className="py-2 px-3 ">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.coagulation')}
+                            </th>
+                            <th className="py-2 px-3 ">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.inflammation')}
+                            </th>
+                            <th className="py-2 px-3 ">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.proliferation')}
+                            </th>
+                            <th className="py-2 px-3 ">{t('MethodologyPage.QuantifySufferingByPain.painTrackTable.total')}</th>
+                            <th className="py-2 px-3 ">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.intensityOfSuffering')} de la
+                                souffrance
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr className="bg-grey  h-12">
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 ">70%</td>
+                            <td className="py-2 px-3 ">196.00</td>
+                            <td className="py-2 px-3  bg-pink-1">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.discomfort')}
+                            </td>
+                        </tr>
+                        <tr className="bg-grey  h-12">
+                            <td className="py-2 px-3 ">50%</td>
+                            <td className="py-2 px-3 ">80%</td>
+                            <td className="py-2 px-3 ">100%</td>
+                            <td className="py-2 px-3 ">30%</td>
+                            <td className="py-2 px-3 ">196.00</td>
+                            <td className="py-2 px-3  bg-pink-2">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.pain')}
+                            </td>
+                        </tr>
+                        <tr className="bg-grey  h-12">
+                            <td className="py-2 px-3 ">50%</td>
+                            <td className="py-2 px-3 ">20%</td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 ">196.00</td>
+                            <td className="py-2 px-3  bg-pink-3">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.suffering')}
+                            </td>
+                        </tr>
+                        <tr className="bg-grey  h-12">
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 "></td>
+                            <td className="py-2 px-3 ">0 min</td>
+                            <td className="py-2 px-3  bg-brown  light-text">
+                                {t('MethodologyPage.QuantifySufferingByPain.painTrackTable.agony')}
+                            </td>
+                        </tr>
+                        <tr className="">
+                            <td className="py-2 px-3 ">0,5–2 min</td>
+                            <td className="py-2 px-3 ">5–15 min</td>
+                            <td className="py-2 px-3 ">16–48h</td>
+                            <td className="py-2 px-3 ">224–36h</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="bg-white flex flex-col md:flex-row items-stretch w-full mb-12 ">
+                <img
+                    src="/analyze-pain.png"
+                    className="w-full md:w-2/4 object-cover"
+                    alt={t('MethodologyPage.QuantifySufferingByPain.conclusionAltImg')}
+                    style={{display: 'block'}}
+                />
+                <div className="flex flex-col gap-y-10 justify-center md:w-2/4 px-6 md:px-12">
+                    <p className="text-md p-2 md:p-0 text-bold">{t('MethodologyPage.QuantifySufferingByPain.conclusionText')}</p>
+                    <div className="w-full flex justify-center">
+                        <CustomLinkButton
+                            href="#list_of_pains"
+                            aria_label={t('MethodologyPage.QuantifySufferingByPain.conclusionButton')}
+                            button_text={t('MethodologyPage.QuantifySufferingByPain.conclusionButton')}
+                            width='small'
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="bg-white flex flex-col md:flex-row items-stretch w-full mb-12 ">
-        <img
-          src="/analyze-pain.png"
-          className="w-full md:w-2/4 object-cover"
-          alt={t('MethodologyPage.QuantifySufferingByPain.conclusionAltImg')}
-          style={{ display: 'block' }}
-        />
-        <div className="flex flex-col gap-y-10 justify-center md:w-2/4 px-6 md:px-12">
-          <p className="text-md p-2 md:p-0 text-bold">{t('MethodologyPage.QuantifySufferingByPain.conclusionText')}</p>
-          <Link
-              href="#list_of_pains"
-              className="text-sm self-center font-mono font-bold py-4 mb-4 px-6 rounded-full shadow-[4px_4px_0_#000] cursor-pointer transition-all duration-200 bg-(--pink-3) hover:bg-(--violet-1)"
-          >
-            {t('MethodologyPage.QuantifySufferingByPain.conclusionButton')}
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- frontend/app/[locale]/ui/general/home-page/go-further-section.tsx
+++ frontend/app/[locale]/ui/general/home-page/go-further-section.tsx
@@ -1,60 +1,44 @@
 import { getI18n } from '@/locales/server';
-import Link from 'next/link';
 import TwitterShareButton from '@/app/actions/share-actions';
 import BoltIcon from "@/app/[locale]/ui/icons/BoltIcon";
+import CustomLinkButton from "@/app/[locale]/ui/general/elements/custom-link-button";
 
 export default async function GoFurtherSection() {
-  const t = await getI18n();
+    const t = await getI18n();
 
-  return (
-    <section id="GoFurtherSection" className="mb-10 p-8 sm:px-16 scroll-mt-18" aria-labelledby="go-further-heading">
-      <div className="flex justify-center mb-6" aria-hidden="true">
-        <div className="bg-violet-2 w-14 h-20 rounded-full flex items-center justify-center shadow-[4px_4px_0_#000]">
-          <BoltIcon color='black'/>
-        </div>
-      </div>
-      <h2
-        id="go-further-heading"
-        className="text-3xl sm:text-4xl font-extrabold tracking-wide dark-text mb-10 text-center"
-      >
-        {t('GoFurther.title')}
-      </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-3xl mx-auto">
-        <form>
-          {/* <form action={downloadMediaKit}> */}
-          {/* <button
-            type="submit"
-            className="w-full bg-(--pink-3) dark-text font-mono font-bold py-4 px-6 rounded-full shadow-[4px_4px_0_#000] cursor-pointer transition-all duration-200 hover:bg-(--violet-1)"
-            aria-label={t('GoFurther.downloadMediaKit')}
-          >
-            {t('GoFurther.downloadMediaKit')}
-          </button> */}
-          <a
-            href="/kit_presse_demo.pdf"
-            download
-            className="w-full text-center dark-text font-mono font-bold py-4 px-6 rounded-full shadow-[4px_4px_0_#000] flex items-center justify-center transition-all duration-200 bg-(--pink-3) hover:bg-(--violet-1)"
-            aria-label={t('GoFurther.downloadMediaKit')}
-          >
-            {t('GoFurther.downloadMediaKit')}
-          </a>
-        </form>
-        <TwitterShareButton nameLien={t('GoFurther.share')} />
-        <Link
-          href="/methodology"
-          className="w-full text-center  dark-text font-mono font-bold py-4 px-6 rounded-full shadow-[4px_4px_0_#000] flex items-center justify-center transition-all duration-200 bg-(--pink-3) hover:bg-(--violet-1)"
-          aria-label={t('GoFurther.methodology')}
-        >
-          {t('GoFurther.methodology')}
-        </Link>
-
-        <Link
-          href="/about"
-          className="w-full text-center  dark-text font-mono font-bold py-4 px-6 rounded-full shadow-[4px_4px_0_#000] flex items-center justify-center transition-all duration-200 bg-(--pink-3) hover:bg-(--violet-1) "
-          aria-label={t('GoFurther.aboutThisWebsite')}
-        >
-          {t('GoFurther.aboutThisWebsite')}
-        </Link>
-      </div>
-    </section>
-  );
+    return (
+        <section id="GoFurtherSection" className="mb-10 p-8 sm:px-16 scroll-mt-18" aria-labelledby="go-further-heading">
+            <div className="flex justify-center mb-6" aria-hidden="true">
+                <div
+                    className="bg-violet-2 w-14 h-20 rounded-full flex items-center justify-center shadow-[4px_4px_0_#000]">
+                    <BoltIcon color='black'/>
+                </div>
+            </div>
+            <h2
+                id="go-further-heading"
+                className="text-3xl sm:text-4xl font-extrabold tracking-wide dark-text mb-10 text-center"
+            >
+                {t('GoFurther.title')}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-3xl mx-auto">
+                <CustomLinkButton
+                    href="/kit_presse_demo.pdf"
+                    aria_label={t('GoFurther.downloadMediaKit')}
+                    button_text={t('GoFurther.downloadMediaKit')}
+                    download={true}
+                />
+                <TwitterShareButton nameLien={t('GoFurther.share')}/>
+                <CustomLinkButton
+                    href="/methodology"
+                    aria_label={t('GoFurther.methodology')}
+                    button_text={t('GoFurther.methodology')}
+                />
+                <CustomLinkButton
+                    href='/about'
+                    aria_label={t('GoFurther.aboutThisWebsite')}
+                    button_text={t('GoFurther.aboutThisWebsite')}
+                />
+            </div>
+        </section>
+    );
 }
