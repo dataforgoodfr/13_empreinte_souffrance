34dc835de86dbc3800439ba69ac752ee4ddaa650
TheoDegorre <degorret@gmail.com>
2025-11-03 13:31:31 +0100

    adding a Image component for img optimisation

--- frontend/app/[locale]/methodology/_components/quantify-pain-by-sources.tsx
+++ frontend/app/[locale]/methodology/_components/quantify-pain-by-sources.tsx
@@ -1,5 +1,6 @@
 import { getI18n } from '@/locales/server';
 import Link from 'next/link';
+import Image from 'next/image';
 import ButtonLink from '@/app/[locale]/ui/_components/button-link';
 import PainTrackTable from './pain-track-table';
 
@@ -9,9 +10,7 @@ export default async function QuantifyPainBySources() {
   return (
     <div className="max-w-[935px] flex flex-col items-center gap-8">
       <hgroup className="max-w-[650px]">
-        <h2 className="font-extrabold mb-4 uppercase ">
-          {t('MethodologyPage.QuantifySufferingByPain.title')}
-        </h2>
+        <h2 className="font-extrabold mb-4 uppercase ">{t('MethodologyPage.QuantifySufferingByPain.title')}</h2>
         <p className="text-md mb-6">{t('MethodologyPage.QuantifySufferingByPain.question')}</p>
         <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description1')}</p>
         <p className="text-md mb-2">{t('MethodologyPage.QuantifySufferingByPain.description2')}</p>
@@ -24,21 +23,31 @@ export default async function QuantifyPainBySources() {
           {t('MethodologyPage.QuantifySufferingByPain.painTrackTableTitle')}
         </h3>
         <hr className="border-1 mb-10 border-pink-3" />
-        <div className="bg-white p-6 rounded-[10px]">
+        <div className="bg-white p-6 rounded-[5px]">
           {/* <PainTrackTable /> */}
-          <img src="/pain-track-table.png" className='w-full'/>
+          <Image
+            src="/pain-track-table.png"
+            width={640}
+            height={840}
+            alt={t('MethodologyPage.QuantifySufferingByPain.painTrackTableTitle')}
+            className="w-full"
+          />
         </div>
       </div>
 
       <figure className="bg-white flex flex-col md:flex-row items-stretch w-full rounded-[5px]">
-        <img
+        <Image
           src="/img_placeholder.png"
-          className="w-full md:w-2/4 object-cover rounded-t-[5px] md:rounded-l-[5px] rounded-b-none md:rounded-r-none"
+          width={640}
+          height={840}
           alt={t('MethodologyPage.QuantifySufferingByPain.conclusionAltImg')}
+          className="w-full rounded-t-[5px] md:rounded-l-[5px] rounded-b-none md:rounded-r-none"
         />
-        <figcaption className="flex flex-col items-center justify-center gap-6 p-6 md:px-12 ">
+        <figcaption className="flex flex-col items-center justify-center gap-6 p-6 md:px-12 md:w-1/2 ">
           <p className="text-body text-bold">{t('MethodologyPage.QuantifySufferingByPain.conclusionText')}</p>
-          <Link className="primary-button text-button w-full" href="#list_of_pains">{t('MethodologyPage.QuantifySufferingByPain.conclusionButton')}</Link>
+          <Link className="primary-button text-button w-full" href="#list_of_pains">
+            {t('MethodologyPage.QuantifySufferingByPain.conclusionButton')}
+          </Link>
         </figcaption>
       </figure>
     </div>
