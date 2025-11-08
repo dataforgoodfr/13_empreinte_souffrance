b9c3924c0422f94eb3d547d0b878fcd2af273f22
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-11-08 18:58:08 +0100

    Update frontend/app/[locale]/ui/_components/nav-class.tsx
    Co-authored-by: Remi Girard <remi.girard@protonmail.com>

--- frontend/app/[locale]/ui/_components/nav-class.tsx
+++ frontend/app/[locale]/ui/_components/nav-class.tsx
@@ -2,21 +2,18 @@
 
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import useLangSuffix from '@/app/[locale]/ui/_hooks/useLangSuffix';
+import matchPath from '@/app/[locale]/ui/_utils/matchPath';
 
 export default function NavClass({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const langSuffix = useLangSuffix();
 
-  // Applique un fond différent selon la route
+  // Apply a different background depending on route
   const navClass = clsx('flex flex-row justify-between w-full h-full', {
-    'bg-pink-2': pathname === '/fr' || pathname === '/',
-    'bg-violet': pathname === '/fr/methodology' || pathname === '/methodology',
-    'bg-white':
-      pathname === '/fr/calculator' ||
-      pathname === '/calculator' ||
-      pathname === '/fr/numbers' ||
-      pathname === '/numbers' ||
-      pathname === '/fr/about' ||
-      pathname === '/about',
+    'bg-pink-2': matchPath('/', pathname, langSuffix),
+    'bg-violet': matchPath('/methodology', pathname, langSuffix),
+    'bg-white': ['/calculator', '/numbers', '/about'].some((routeName) => matchPath(routeName, pathname, langSuffix)),
   });
 
   return <nav className={navClass}>{children}</nav>;
