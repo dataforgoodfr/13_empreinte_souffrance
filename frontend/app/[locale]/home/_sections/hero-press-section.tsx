ccdb0b8992381a553fd0e6bd0505d19d0294cadc
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-11-10 11:03:15 +0100

    Fix/navbar styles and pages (#243)
    * fixing navbar bg-color, fixing files architecture, create folder for numbers page
    
    * fixing dynamique bg
    
    * fix nav-class.tsx components with matchPath

--- frontend/app/[locale]/home/_sections/hero-press-section.tsx
+++ frontend/app/[locale]/home/_sections/hero-press-section.tsx
@@ -2,6 +2,7 @@ import { getI18n } from '@/locales/server';
 import Image from 'next/image';
 import ArrowDown from '@/app/[locale]/ui/_logo/ArrowDown';
 
+
 export default async function HeroPressSection() {
   const t = await getI18n();
 
