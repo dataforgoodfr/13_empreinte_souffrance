8649e8e196a302b25d3850a84b683d2ee6c0afc5
Théo Degorre <136261688+TheoDegorre@users.noreply.github.com>
2025-11-10 16:33:53 +0100

    Fix/about sections (#246)
    * fixing style for about pages, all sections
    
    * fixing marging
    
    * initiating sources_section
    
    * fix eslin error unsued scopedT for traduction

--- frontend/app/[locale]/about/page.tsx
+++ frontend/app/[locale]/about/page.tsx
@@ -1,6 +1,8 @@
 import ThankingSection from './_sections/thanking_section';
 import WhyNameSection from './_sections/why_name_section';
 import AssociationPresentationSection from './_sections/association-presentation-section';
+import GoFurtherSection from '../ui/_sections/go-further-section';
+import SourcesSection from './_sections/sources_section';
 
 export default async function About() {
   return (
@@ -8,6 +10,8 @@ export default async function About() {
       <AssociationPresentationSection />
       <WhyNameSection />
       <ThankingSection />
+      <SourcesSection/>
+      <GoFurtherSection/>
     </>
   );
 }
