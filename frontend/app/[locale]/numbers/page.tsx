50d2ce6f0ceb44bc7d675b1c0574773b3392055e
LucBERTON <luc.jp.berton@gmail.com>
2025-11-11 17:08:35 +0100

    dashboard commitment section

--- frontend/app/[locale]/numbers/page.tsx
+++ frontend/app/[locale]/numbers/page.tsx
@@ -1,7 +1,10 @@
+import CommitmentSection from './_sections/commitment-section';
+
 export default async function NumbersPage() {
   return (
-    <section className="flex justify-center">
+    <section className="flex flex-col justify-center">
       <h1>Cette page est cours de construction </h1>
+      <CommitmentSection />
     </section>
   );
 }
