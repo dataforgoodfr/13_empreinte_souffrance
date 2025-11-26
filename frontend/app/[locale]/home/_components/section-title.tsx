ebffe62be401dfef79cc1547e16c49c4fbf0bc90
LucBERTON <luc.jp.berton@gmail.com>
2025-11-26 21:08:34 +0100

    section title update to manage text color and use in go further

--- frontend/app/[locale]/home/_components/section-title.tsx
+++ frontend/app/[locale]/home/_components/section-title.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 import { ReactNode } from 'react';
 
@@ -5,13 +6,22 @@ type SectionTitleProps = {
   image_path: string;
   image_alt: string;
   title: ReactNode;
+  text_color?: 'white' | 'black';
 };
 
-export default function SectionTitle({ image_path, image_alt, title }: SectionTitleProps) {
+export default function SectionTitle({ image_path, image_alt, title, text_color = 'black' }: SectionTitleProps) {
   return (
     <div className="flex flex-col items-center justify-center flex-grow pt-2">
       <Image src={image_path} width={200} height={200} alt={image_alt} className="mb-0 md:mb-0" />
-      <h1 className="text-center font-bold text-lg md:text-sm leading-snug text-black px-4 mt-[-1rem] mx-10 sm:mx-25 md:mx-45">
+      <h1
+        className={clsx(
+          'text-center font-bold text-lg md:text-sm leading-snug px-4 mt-[-1rem] mx-10 sm:mx-25 md:mx-45',
+          {
+            'text-black': text_color == 'black',
+            'text-white': text_color == 'white',
+          }
+        )}
+      >
         {title}
       </h1>
     </div>
