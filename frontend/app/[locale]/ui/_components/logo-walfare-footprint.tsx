464fcab1bcdf2f5d93fe3abfbcd0a0a609ac0f82
LucBERTON <luc.jp.berton@gmail.com>
2025-12-09 17:41:42 +0100

    fix logo name

--- frontend/app/[locale]/ui/_components/logo-walfare-footprint.tsx
+++ frontend/app/[locale]/ui/_components/logo-walfare-footprint.tsx
@@ -4,7 +4,13 @@ import Image from 'next/image';
 export default async function Logo() {
   return (
     <Link href="/" className="gap-2 md:gap-1 flex md:flex-row items-center md:p-2 text-bold text-black uppercase">
-      <Image src="/logotype.svg" width={215} height={50} alt="Logo Anima" className="mb-0 md:mb-0 object-contain" />
+      <Image
+        src="/logotype-heure-comptes.svg"
+        width={215}
+        height={50}
+        alt="Logo Anima"
+        className="mb-0 md:mb-0 object-contain"
+      />
     </Link>
   );
 }
