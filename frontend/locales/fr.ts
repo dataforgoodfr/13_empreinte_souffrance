cbe5e54ff290316ecbea13281e5edec2fd30ea52
LucBERTON <luc.jp.berton@gmail.com>
2025-12-23 17:46:21 +0100

    ongoing text edit

--- frontend/locales/fr.ts
+++ frontend/locales/fr.ts
@@ -702,7 +702,7 @@ export default {
   aboutPage: {
     association_presentation_section: {
       title:
-        "L'Heure des Comptes est le fruit d’une collaboration entre Anima et Data for Good, deux associations à but non lucratif.",
+        "L'Heure des Comptes est le fruit d’une collaboration entre Anima et Data for Good.",
       anima_presentation:
         'Anima existe pour réduire la souffrance des animaux de la façon la plus efficace possible. L’association mène une campagne nationale et européenne pour mettre un terme à l’élevage en cage des poules pondeuses. Anima est membre de la coalition européenne Anima International.',
       anima_link: 'www.animafrance.org',
