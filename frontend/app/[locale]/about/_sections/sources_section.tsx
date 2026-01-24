f611e9ed064fe5b977838bb417cb3e315517221b
TheoDegorre <degorret@gmail.com>
2026-01-24 15:34:22 +0100

    removing empty sources section, all sources will be linked directly

--- frontend/app/[locale]/about/_sections/sources_section.tsx
+++
@@ -1,8 +0,0 @@
-export default async function SourcesSection() {
-  return (
-    <section className="p-section flex flex-col items-center justify-center bg-grey text-black gap-[28px]">
-      <h1>SOURCES</h1>
-      <p className="lg:max-w-2/3 body">section placeholer</p>
-    </section>
-  );
-}
