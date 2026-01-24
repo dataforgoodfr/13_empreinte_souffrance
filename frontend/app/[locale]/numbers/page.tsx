153d02757e88824f75e22fe758d0564c9a7b7a96
TheoDegorre <degorret@gmail.com>
2026-01-24 16:31:21 +0100

    numbers page gofurtherpage fixed

--- frontend/app/[locale]/numbers/page.tsx
+++ frontend/app/[locale]/numbers/page.tsx
@@ -7,37 +7,37 @@ import StoreMapClient from '@/app/[locale]/numbers/_components/store-map-client'
 
 export default async function NumbersPage() {
   return (
-    <section className="flex flex-col items-center gap-8">
+    <>
       <Header />
-      <Section
-        title="DATES DES ENGAGEMENTS HORS CAGE DES SUPERMARCHES"
-        text={
-          'Il y a 10 ans, les supermarchés s’engageaient à ne plus vendre aucun œuf de poule en cage d’ici fin 2025. Ces engagements concernent aussi bien les oeufs frais, que les oeufs utilisés dans les produits à marque distributeur (pâtes fraîches aux oeufs, brioche, gâteaux…etc).'
-        }
-      >
-        <Supermarkets className="w-full h-full" />
-      </Section>
-       <hr className='border border-pink-3 w-full max-w-[250px] md:max-w-[620px]'/>
-      <Section
-        title={'SUPERMARCHES VENDANT ENCORE DES ŒUFS CAGE'}
-        text={
-          'Une grande enquête menée en janvier 2026 a montré que xxx supermachés continuaient de vendre des œufs cage. Pourtant, plus aucun oeuf cage ne devrait se trouver en supermarché selon les engagements.'
-        }
- 
-      >
-        <CagedEggsGraph />
-      </Section>
-       <hr className='border border-pink-3 w-full max-w-[250px] md:max-w-[620px]'/>
-      <Section
-        title={'PART D’OEUFS CAGE EN SUPERMARCHES'}
-        text={
-          'La cage occupait encore une part minoriaire mais signifcative en 2025. Le plein air est le mode délevage le plus répandu en supermarché. '
-        }
-      >
-        <StoreMapClient />
-      </Section>
-      <hr className='border border-pink-3 w-full max-w-[250px] md:max-w-[620px]'/>
+      <section className="flex flex-col items-center gap-8">
+        <Section
+          title="DATES DES ENGAGEMENTS HORS CAGE DES SUPERMARCHES"
+          text={
+            'Il y a 10 ans, les supermarchés s’engageaient à ne plus vendre aucun œuf de poule en cage d’ici fin 2025. Ces engagements concernent aussi bien les oeufs frais, que les oeufs utilisés dans les produits à marque distributeur (pâtes fraîches aux oeufs, brioche, gâteaux…etc).'
+          }
+        >
+          <Supermarkets className="w-full h-full" />
+        </Section>
+        <hr className="border border-pink-3 w-full max-w-[250px] md:max-w-[620px]" />
+        <Section
+          title={'SUPERMARCHES VENDANT ENCORE DES ŒUFS CAGE'}
+          text={
+            'Une grande enquête menée en janvier 2026 a montré que xxx supermachés continuaient de vendre des œufs cage. Pourtant, plus aucun oeuf cage ne devrait se trouver en supermarché selon les engagements.'
+          }
+        >
+          <CagedEggsGraph />
+        </Section>
+        <hr className="border border-pink-3 w-full max-w-[250px] md:max-w-[620px]" />
+        <Section
+          title={'PART D’OEUFS CAGE EN SUPERMARCHES'}
+          text={
+            'La cage occupait encore une part minoriaire mais signifcative en 2025. Le plein air est le mode délevage le plus répandu en supermarché. '
+          }
+        >
+          <StoreMapClient />
+        </Section>
+      </section>
       <GoFurtherSection />
-    </section>
+    </>
   );
 }
