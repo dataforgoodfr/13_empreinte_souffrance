1a35217aef70bd5946a255296ddd71ac5fd62732
LucBERTON <luc.jp.berton@gmail.com>
2026-02-14 11:42:09 +0100

    update background color and numbers page header

--- frontend/app/[locale]/numbers/_components/Header.tsx
+++ frontend/app/[locale]/numbers/_components/Header.tsx
@@ -1,10 +1,7 @@
-import EggInterrogation from '@/app/[locale]/numbers/_components/EggInterrogation';
-
 export default async function Header() {
   return (
-    <div className="w-full flex flex-col items-center">
-      <EggInterrogation />
-      <h2 className="relative z-1 text-center font-albert-sans font-black text-brown" style={{ top: '-4rem' }}>
+    <div className="w-full flex flex-col items-center bg-pink-2">
+      <h2 className="relative z-1 text-center font-albert-sans font-black text-brown py-20">
         {'Les chiffres derrière l’histoire'}
       </h2>
     </div>
