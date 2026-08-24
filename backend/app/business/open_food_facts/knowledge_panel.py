8738025eae3ce69a9c04e53596462b2cd4be8f98
Marie Groshens <165828560+littlecoffe8@users.noreply.github.com>
2026-08-24 14:22:04 +0200

    mock-for-new-off-explorer

--- backend/app/business/open_food_facts/knowledge_panel.py
+++ backend/app/business/open_food_facts/knowledge_panel.py
@@ -1,7 +1,10 @@
+import json
 import logging
+from pathlib import Path
 from typing import Callable
 
 import httpx
+from fastapi.responses import JSONResponse
 from pydantic import HttpUrl, ValidationError
 
 from app.business.open_food_facts.pain_report_calculator import PainReportCalculator
@@ -172,8 +175,67 @@ def get_knowledge_panel_response(
         A complete KnowledgePanelResponse containing root panel, intensity definitions,
         physical pain data and psychological pain data
     """
-    panel_generator = KnowledgePanelGenerator(pain_report, translator)
-    return panel_generator.get_response()
+
+    def import_html_body(path: str) -> JSONResponse:
+        """
+        Import the html body from a given path
+        """
+        from bs4 import BeautifulSoup
+
+        with open(path, "r", encoding="utf-8") as f:
+            html_content = f.read()
+
+        soup = BeautifulSoup(html_content, "html.parser")
+        body = soup.body
+
+        return body.decode_contents() if body else ""
+
+    def replace_html_body(obj):
+        ''' replaces {html_body} in all strings in json object
+        with body_content extracted from knowledge_panel.html '''
+        if isinstance(obj, dict):
+            return {k: replace_html_body(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [replace_html_body(v) for v in obj]
+        elif isinstance(obj, str):
+            return obj.replace("{html_body}", html_content)
+        return obj
+
+
+
+    json_path = Path(__file__).resolve().parent / "data" / "knowledge_panel_project.json"
+    with open(json_path, "r", encoding="utf-8") as f:
+        raw_knowledge_panel =  json.load(f)
+
+    html_path = Path(__file__).resolve().parent / "data" / "root_panel_complete.html"
+
+    html_content = import_html_body(html_path)
+
+    knowledge_panel = replace_html_body(raw_knowledge_panel)
+
+
+
+    def replace_html_body_project(obj):
+        ''' replaces {html_body} in all strings in json object
+        with body_content extracted from knowledge_panel.html '''
+        if isinstance(obj, dict):
+            return {k: replace_html_body_project(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [replace_html_body_project(v) for v in obj]
+        elif isinstance(obj, str):
+            return obj.replace("{html_body_project}", html_content_project)
+        return obj
+    
+    html_content_project = import_html_body(Path(__file__).resolve().parent / "data" / "about_the_project_panel.html")
+
+    knowledge_panel = replace_html_body_project(knowledge_panel)
+
+    knowledge_panel_json = JSONResponse(content=knowledge_panel)
+
+    return knowledge_panel_json
+
+
+
 
 
 class KnowledgePanelGenerator:
